1eddbd89cf82caca45427bca08aa54409eba5659
Juan Galarce <j6alarce@gmail.com>
2021-03-02 16:07:06 -0300

    Mostrando la letra de la cancion en el componente Song

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { Fragment, useState, useEffect } from "react";
 import Form from "./Components/Form";
-import axios from 'axios';
+import Song from './Components/Song'
+import axios from "axios";
 
 function App() {
   const [searchLetter, setSearchLetter] = useState({});
-  const [lyrics, setLyrics] = useState('')
+  const [lyrics, setLyrics] = useState("");
 
   useEffect(() => {
     // Si el objeto esta vacio
     if (Object.keys(searchLetter).length === 0) return;
 
-    const consultApiLyrics = async() => {
+    const consultApiLyrics = async () => {
       const { artist, song } = searchLetter;
       const url = `https://api.lyrics.ovh/v1/${artist}/${song}`;
 
       const result = await axios(url);
 
       setLyrics(result.data.lyrics);
-
     };
 
     consultApiLyrics();
@@ -26,6 +26,15 @@ function App() {
   return (
     <Fragment>
       <Form setSearchLetter={setSearchLetter} />
+
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6"></div>
+          <div className="col-md-6">
+            <Song lyrics={lyrics}/>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
