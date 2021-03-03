13eed202313dcd9f42ac3646805a784655e3073d
Juan Galarce <j6alarce@gmail.com>
2021-03-03 11:45:27 -0300

    Mostrando la informacion del artista, proyecto terminado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment, useState, useEffect } from "react";
 import Form from "./Components/Form";
 import Song from "./Components/Song";
+import Info from "./Components/Info";
+
 import axios from "axios";
 
 function App() {
   const [searchLetter, setSearchLetter] = useState({});
-  const [lyrics, setLyrics ] = useState("");
+  const [lyrics, setLyrics] = useState("");
   const [info, setInfo] = useState({});
 
   useEffect(() => {
@@ -17,27 +19,26 @@ function App() {
       const url = `https://api.lyrics.ovh/v1/${artist}/${song}`;
       const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
 
-      const [lyrics, info] = await Promise.all([
-        axios(url),
-        axios(url2),
-      ]);
+      const [lyrics, info] = await Promise.all([axios(url), axios(url2)]);
 
       setLyrics(lyrics.data.lyrics);
-      console.log(info.data.artists[0].strArtist);
+      setInfo(info.data.artists[0]);
 
-     // setLyrics(result.data.lyrics);
+      // setLyrics(result.data.lyrics);
     };
 
     consultApiLyrics();
-  }, [searchLetter]);
+  }, [searchLetter, info]);
 
   return (
     <Fragment>
       <Form setSearchLetter={setSearchLetter} />
 
-      <div className="container mt-5">
+      <div className="container mt-5 mb-5">
         <div className="row">
-          <div className="col-md-6"></div>
+          <div className="col-md-6">
+            <Info info={info} />
+          </div>
           <div className="col-md-6">
             <Song lyrics={lyrics} />
           </div>
